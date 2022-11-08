f89cd607c7552c44ca05c7b6422d6e803decbe9a
nikitapryymak <nikitapryymak29@gmail.com>
2022-11-07 18:35:29 -0700

    Finish app

---
+++ config/nodemailer.js
@@ -0,0 +1,17 @@
+import nodemailer from "nodemailer";
+
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass,
+  },
+});
+
+export const mailOptions = {
+  from: email,
+  to: email,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,156 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { sendContactForm } from "../lib/api";
+
+const initValues = {
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
+
+const initState = { isLoading: false, error: "", values: initValues };
 
 export default function Home() {
-  return <Heading>Contact</Heading>;
+  const toast = useToast();
+  const [state, setState] = useState(initState);
+  const [touched, setTouched] = useState({});
+
+  const { values, isLoading, error } = state;
+
+  const onBlur = ({ target }) => {
+    setTouched((prev) => ({ ...prev, [target.name]: true }));
+  };
+
+  const handleChange = ({ target }) =>
+    setState((prev) => ({
+      ...prev,
+      values: { ...prev.values, [target.name]: target.value },
+    }));
+
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    try {
+      await sendContactForm(values);
+      setTouched({});
+      setState(initState);
+      toast({
+        title: "Message sent.",
+        status: "success",
+        duration: 2000,
+        position: "top",
+      });
+    } catch (error) {
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+        error: error.message,
+      }));
+    }
+  };
+
+  return (
+    <Container maxW="450px" mt={12}>
+      <Heading>Contact</Heading>
+      {error && (
+        <Text color="red.300" my={4} fontSize="xl">
+          {error}
+        </Text>
+      )}
+
+      <FormControl isRequired isInvalid={touched.name && !values.name} mb={5}>
+        <FormLabel>Name</FormLabel>
+        <Input
+          type="text"
+          name="name"
+          errorBorderColor="red.300"
+          value={values.name}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        {touched.name && !values.name && (
+          <FormErrorMessage>Required</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          name="email"
+          errorBorderColor="red.300"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        {touched.email && !values.email && (
+          <FormErrorMessage>Required</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl
+        mb={5}
+        isRequired
+        isInvalid={touched.subject && !values.subject}
+      >
+        <FormLabel>Subject</FormLabel>
+        <Input
+          type="text"
+          name="subject"
+          value={values.subject}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        {touched.subject && !values.subject && (
+          <FormErrorMessage>Required</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl
+        isRequired
+        isInvalid={touched.message && !values.message}
+        mb={5}
+      >
+        <FormLabel>Message</FormLabel>
+        <Textarea
+          type="text"
+          name="message"
+          rows={4}
+          errorBorderColor="red.300"
+          value={values.message}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        {touched.message && !values.message && (
+          <FormErrorMessage>Required</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <Button
+        variant="outline"
+        colorScheme="blue"
+        isLoading={isLoading}
+        disabled={
+          !values.name || !values.email || !values.subject || !values.message
+        }
+        onClick={onSubmit}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
