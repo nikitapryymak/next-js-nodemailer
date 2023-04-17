61498bd65f700d9f072f8cec43d020115aa1c1ce
Sebastian Przybylski <generalg2003@gmail.com>
2023-04-17 15:17:45 -0500

    added comments to contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,43 +1,86 @@
+/**
+ * In Next.js, all files within the api folder is treated as an API endpoint instead of a page.
+ * This allows us to seemesly write backend code without interupting the processes of frontend.
+ * I reccomend vercel's documentation on this: https://nextjs.org/docs/api-routes/introduction
+ * contact.js serves the purpose of:
+ * 1) receiving the form data
+ * 2) constructing an email with said data
+ * 3) sending the data to the proper email endpoint
+ * 
+ * Here is another article I highly reccomend reading: https://refine.dev/blog/next-js-api-routes/
+ */
+
+//import the nodemailer objects we configured earlier
 import { mailOptions, transporter } from "../../config/nodemailer";
 
-const CONTACT_MESSAGE_FIELDS = {
-  name: "Name",
-  email: "Email",
-  subject: "Subject",
-  message: "Message",
-};
-
+/**
+ * (I reccomend you read the comments below this function first and come back to this paragraph, it'll
+ * probably make more sense then)
+ * 
+ * This function accepts the arguement 'data', as parsed from /lib/api.js, and then accumaltes the
+ * data therewithin to print a long string of data
+ */
 const generateEmailContent = (data) => {
+  /**
+   * 'stringData' is our accumlator variables, we will append text to it recursivley until it is done.
+   */
   const stringData = Object.entries(data).reduce(
     (str, [key, val]) =>
-      (str += `${CONTACT_MESSAGE_FIELDS[key]}: \n${val} \n \n`),
-    ""
-  );
-  const htmlData = Object.entries(data).reduce((str, [key, val]) => {
-    return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
-  }, "");
-
+      (str += `${key}: \n${val} \n \n`),"");
   return {
     text: stringData,
-    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+    //optional html data
   };
 };
 
+/**
+ * This 'handler' function is responsible for handling the requests made to this endpoint (contact.js, that is).
+ * It has two arguements: 'req' and 'res' (these should be familiar if you've ever worked with Express.js or
+ * Node). 'req' is an instance of the incoming HTTP request, and 'res' is an instance of the HTTP server 
+ * response. These two instances come bundled with some convientnet middleware that allows us to parse &
+ * manipulate our data better.
+ */
 const handler = async (req, res) => {
+  /**
+   * Check if our HTTP request is a POST method. If it is - execute the code within the function.
+   * Else, return a 400 HTTP error code of 'bad request'. In other words: "if this ain't a POST
+   * method, something has gone wrong - don't execute the code any further and tell the server
+   * there has been some sort of bad request error"
+   */
   if (req.method === "POST") {
+    //'req.body' correlates to the whole contents of our request (which is the contents of 'values' from index.jsx)
     const data = req.body;
+    //Send an error response if some piece of the body code is empty (in the case someone submitted missing info somehow)
     if (!data || !data.name || !data.email || !data.subject || !data.message) {
       return res.status(400).send({ message: "Bad request" });
     }
 
+    /**
+     * Our 'transporter' object was already constructed in /config/nodemailer, remember it is a 
+     * nodemailer object. We call the 'sendMail' function from nodemailer to actually send our 
+     * data. We put within a try/catch block to try to send the mail first, and to catch &
+     * errors we encoutner, and then return a bad status with a json message reporting the error.
+     * We 'await' transporter.sendMail() because our handler function is asynchronous, and we need
+     * to wait to return our promise.
+     */
     try {
       await transporter.sendMail({
+        //spread out mailOptions, which if you'll remember, is a data structure containing who the mail is from, and to who its going to
         ...mailOptions,
+        /**
+         * Call the generateEmailContent, which accepts data as its arguement (which we passed in /lib/api.js).
+         * The function will return our the value of the nodemailer 'text' property (which needs to be to be
+         * filled non empty if we want the email sent to actually contain the form data). This function can
+         * also be configured to return HTML elements as well, as more thoroughly explained in the
+         * video tutorial in index.js.
+         */
         ...generateEmailContent(data),
+        //The 'subject' property is a nodemailer property that fills the subject of the email. We pass it with our 'subject' field from 'data'.
         subject: data.subject,
       });
-
+      //If the email was sent A-OK: send a HTTP 200 response of "everything went good"
       return res.status(200).json({ success: true });
+      //catch the error, log it, and display its JSON contents:
     } catch (err) {
       console.log(err);
       return res.status(400).json({ message: err.message });
@@ -45,4 +88,5 @@ const handler = async (req, res) => {
   }
   return res.status(400).json({ message: "Bad request" });
 };
+
 export default handler;
