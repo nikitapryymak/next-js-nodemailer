3ec9bb6634df7815e01162a812da184a20c50d83
Sebastian Przybylski <generalg2003@gmail.com>
2023-04-17 14:30:48 -0500

    removed chakra referecnes in index.js

--- pages/index.js
+++ pages/index.js
@@ -61,86 +61,55 @@ export default function Home() {
   };
 
   return (
-    <Container maxW="450px" mt={12}>
-      <Heading>Contact</Heading>
-      {error && (
-        <Text color="red.300" my={4} fontSize="xl">
-          {error}
-        </Text>
-      )}
-
-      <FormControl isRequired isInvalid={touched.name && !values.name} mb={5}>
-        <FormLabel>Name</FormLabel>
-        <Input
-          type="text"
-          name="name"
-          errorBorderColor="red.300"
-          value={values.name}
-          onChange={handleChange}
-          onBlur={onBlur}
-        />
-        <FormErrorMessage>Required</FormErrorMessage>
-      </FormControl>
-
-      <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
-        <FormLabel>Email</FormLabel>
-        <Input
-          type="email"
-          name="email"
-          errorBorderColor="red.300"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={onBlur}
-        />
-        <FormErrorMessage>Required</FormErrorMessage>
-      </FormControl>
-
-      <FormControl
-        mb={5}
-        isRequired
-        isInvalid={touched.subject && !values.subject}
-      >
-        <FormLabel>Subject</FormLabel>
-        <Input
-          type="text"
-          name="subject"
-          errorBorderColor="red.300"
-          value={values.subject}
-          onChange={handleChange}
-          onBlur={onBlur}
-        />
-        <FormErrorMessage>Required</FormErrorMessage>
-      </FormControl>
-
-      <FormControl
-        isRequired
-        isInvalid={touched.message && !values.message}
-        mb={5}
-      >
-        <FormLabel>Message</FormLabel>
-        <Textarea
-          type="text"
-          name="message"
-          rows={4}
-          errorBorderColor="red.300"
-          value={values.message}
-          onChange={handleChange}
-          onBlur={onBlur}
-        />
-        <FormErrorMessage>Required</FormErrorMessage>
-      </FormControl>
-
-      <Button
-        variant="outline"
-        colorScheme="blue"
-        isLoading={isLoading}
-        disabled={
-          !values.name || !values.email || !values.subject || !values.message
-        }
-        onClick={onSubmit}
-      >
-        Submit
-      </Button>
-    </Container>
-  );
+    <>
+      <Head>
+        <title>Email tester</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {
+        /**
+         * The code below is a simple HTML form that does NOT rely upon any UI components. Take notice of
+         * each input tag. They each have a 'value' field that is equal to 'value.X', where 'X' the tag's
+         * name. Remember what the variable 'value' is: it is a reference to the state property it the
+         * useState instance. We want the value of each tag to be equal to the value of the current 
+         * state, which was called 'state'. Because 'values' is equal to the current state, we can
+         * reference the fields of initValues directly through the variable 'values', as opposed of
+         * doing 'state.values' every time. 
+         * 
+         * However, input tags are not able to change their state on their own, so each input tag
+         * also needs an 'onChange' field to able to update their state. Each input calls the
+         * function 'handleChange' every time it is changed.
+         */
+      }
+      <main>
+       <h1>Contact:</h1>
+       <form>
+        <fieldset>
+          <legend>Input fields:</legend>
+          <div>
+            <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" value={values.name} onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="subject">Subject:</label>
+            <input type="text" id="sub" name="subject" value={values.subject} onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={values.email} onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="message">Message:</label>
+            <textarea type="text" id="message" name="message" value={values.message} onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="submit">Submit info:</label>
+            <button onClick={onSubmit} disabled={!values.name || !values.subject || !values.email || !values.message}>Submit</button>
+          </div>
+        </fieldset>
+      </form>
+      </main>
+    </>
+  )
 }
