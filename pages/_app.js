94324f07078ad4558ea38ae4ae3294a3ec103b95
Sebastian Przybylski <generalg2003@gmail.com>
2023-04-17 14:28:49 -0500

    removed components & chakra ui framework

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import AppContainer from "../components/AppContainer";
-import theme from "../config/theme";
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <AppContainer>
-        <Component {...pageProps} />
-      </AppContainer>
-    </ChakraProvider>
+      <Component {...pageProps} />
   );
 }
 
